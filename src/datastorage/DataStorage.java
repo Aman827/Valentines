aceea1c762ced3bd9a25846efae8c47124d0af05
Andrew <andrewjmason@gmail.com>
2019-01-20 16:14:58 -0600

    Image moving code somewhat implemented, doesn't work without manual input, TBF

--- src/datastorage/DataStorage.java
+++ src/datastorage/DataStorage.java
@@ -10,7 +10,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import static java.lang.System.out;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class DataStorage {
 
@@ -19,8 +21,8 @@ public class DataStorage {
 
     public static void main(String[] args) throws IOException {
         DataStorage bob = new DataStorage();
-        // bob.Save("h7", "j");
-        bob.Reader();
+        bob.Save("h7", "j");
+        //bob.Reader();
     }
 
     private void Save(String message, String WhereMet) throws IOException {
@@ -30,11 +32,11 @@ public class DataStorage {
 
         File studentsValentines = new File(mainDir, email);
         studentsValentines.mkdir();     // creates sub(?) directory inside of the ValentineList Directory, named with the Valentine Recipients' email
-        out.println(studentsValentines.getPath());
+        // out.println(studentsValentines.getPath());
         File individVal = new File(studentsValentines, userEmail);
         individVal.mkdir();     // Creates a third folder, inside of the recipient directory, named with the users email
-        userEmail = userEmail + ".txt";     // sets the user's email into text doc. ready form
-        File individValText = new File(individVal, userEmail);
+        String userEmailTxt = userEmail + ".txt";     // sets the user's email into text doc. ready form
+        File individValText = new File(individVal, userEmailTxt);
 
         individValText.createNewFile();     //creates a text file with the user's email as the name, inside of the directory named with the recipient's email
         FileWriter fileWriter = new FileWriter(individValText.getPath(), true);
@@ -43,6 +45,22 @@ public class DataStorage {
         printWriter.println(message);       // writes to the Text File
         bufferedWriter.close();     //closes the bufferedWriter
         printWriter.close();        //closes the printWriter
+        String usEmImg =  userEmail + ".jpg";
+        File imageDir = new File("Images");
+        imageDir.mkdir();
+        
+        File imageEmail = new File(imageDir, usEmImg);
+         out.println(imageEmail.getPath());
+         
+       
+        File a = new File(imageEmail.getPath());
+        out.println("a     " + a.getPath());
+        a.renameTo(new File(individVal.getPath() + "\\" + a.getName()));
+        out.println(a.getPath() + "        new a path");
+        out.println(individVal.getPath());
+
+        out.println(a.getName() + "      a Name");
+        out.println(a.getPath());
 
     }
 
@@ -53,7 +71,7 @@ public class DataStorage {
         for (int x = 0; x < filelist.length; x++) {
             out.println(filelist[x]);
             File textfileRet = new File(filename + "\\" + filelist[x]);
-            
+
             String[] inFileList = textfileRet.list();
             out.println(inFileList[x]);
             File file = new File(textfileRet.getPath(), inFileList[x]);
